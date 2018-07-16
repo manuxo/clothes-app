a647dca74605d59cd24824f9629dac16361b6c67
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-07-16 02:54:17 -0500

    Static file index.html served

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+//Dependencies
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+//Init App
 const PORT = process.env.PORT || 3000;
-
-
+const IP = process.env.IP || 'localhost';
 const app = express();
+
+//Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname,'dist','clothes-app')));
 
 app.use(morgan('dev'));
 
+//Routes
 
-app.listen(PORT, () => {
-    console.log('Server is running!');
+app.get('*', (req,res,next) => {
+    res.sendFile(path.join(__dirname,'dist','clothes-app'));
+});
+
+app.listen(PORT,IP, () => {
+    console.log(`Server is running on http://${IP}:${PORT}`);
 });
